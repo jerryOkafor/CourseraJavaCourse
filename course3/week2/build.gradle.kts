fb90b11000f17d7fe762ad9d3638b9e56f1d6a61
Jerry Okafor <jerryhanksokafor@gmail.com>
2021-07-20 20:38:23 +0100

    add unversioned files

---
+++ course3/week2/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    kotlin("jvm")
+}
+
+group = "edu.duke.course3.week2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":core"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
